1cff2f443431a43b08c7ffa5e26cb4f0eb41c4d8
linzhibo <lzbzhuanshu@163.com>
2016-09-06 23:35:30 +0800

    First commit of PWeather

---
+++ app/src/main/java/com/linzb/pweather/PWeatherActivity.java
@@ -0,0 +1,13 @@
+package com.linzb.pweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pweather);
+    }
+}
